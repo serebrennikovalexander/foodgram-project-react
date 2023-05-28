a00a952268652e41b2dcb47fd7f2749864c76cda
Александр Серебренников <serebrenn1kov.alexander@yandex.ru>
2023-05-28 21:46:43 +0300

    First review commit 2

--- backend/foodgram_backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/foodgram_backend/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'django_filters',
     'recipes.apps.RecipesConfig',
     'users.apps.UsersConfig',
     'api.apps.ApiConfig',
