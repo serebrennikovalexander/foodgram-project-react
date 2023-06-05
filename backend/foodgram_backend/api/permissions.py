ba7a5330f79a58bf7e341b1ce55e583497c43eed
Александр Серебренников <serebrenn1kov.alexander@yandex.ru>
2023-06-05 21:47:54 +0300

    Second review check project work on server

--- backend/foodgram_backend/api/permissions.py
+++ backend/foodgram_backend/api/permissions.py
@@ -16,4 +16,5 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
-        )   or obj.author == request.user
+            or obj.author == request.user
+        )
