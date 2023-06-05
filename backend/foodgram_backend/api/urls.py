7cbcb1b6fd06475cd4cc8fb369cbb33a530cda29
Александр Серебренников <serebrenn1kov.alexander@yandex.ru>
2023-06-06 02:46:06 +0300

    Second review check project work on server

--- backend/foodgram_backend/api/urls.py
+++ backend/foodgram_backend/api/urls.py
@@ -14,7 +14,7 @@ router.register('ingredients', MainIngredientViewSet, basename='ingredients')
 router.register('users', CustomUserViewSet, basename='users')
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('', include('djoser.urls')),
+    path('', include(router.urls)),
     path('auth/', include('djoser.urls.authtoken')),
 ]
