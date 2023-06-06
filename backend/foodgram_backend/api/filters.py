c0fde21411cdbbb1b4d87d8c102de5c1bf55708e
Александр Серебренников <serebrenn1kov.alexander@yandex.ru>
2023-06-06 21:40:55 +0300

    Second review check project work on server

--- backend/foodgram_backend/api/filters.py
+++ backend/foodgram_backend/api/filters.py
@@ -1,4 +1,5 @@
-from django_filters.rest_framework import FilterSet, filters
+import django_filters
+from django_filters import rest_framework
 from rest_framework.filters import SearchFilter
 
 from recipes.models import MainTag, Recipe
@@ -11,19 +12,20 @@ class IngredientSearchFilter(SearchFilter):
     search_param = 'name'
 
 
-class RecipeFilterSet(FilterSet):
+class RecipeFilterSet(rest_framework.FilterSet):
     """
     Фильтр для рецептов.
     """
-    tags = filters.ModelMultipleChoiceFilter(
+    author = rest_framework.NumberFilter(field_name='author__id')
+    tags = django_filters.ModelMultipleChoiceFilter(
         field_name='tags__slug',
         to_field_name='slug',
         queryset=MainTag.objects.all()
     )
-    is_favorited = filters.BooleanFilter(
+    is_favorited = django_filters.NumberFilter(
         method='is_favorited_filter'
     )
-    is_in_shopping_cart = filters.BooleanFilter(
+    is_in_shopping_cart = django_filters.NumberFilter(
         method='is_in_shopping_cart_filter'
     )
 
@@ -37,11 +39,15 @@ class RecipeFilterSet(FilterSet):
         )
 
     def is_favorited_filter(self, queryset, name, value):
-        if value and self.request.user.is_authenticated:
+        if value == 1:
             return queryset.filter(favorites__user=self.request.user)
-        return queryset
+        # if value and self.request.user.is_authenticated:
+            # return queryset.filter(favorites__user=self.request.user)
+        # return queryset
 
     def is_in_shopping_cart_filter(self, queryset, name, value):
-        if value and self.request.user.is_authenticated:
+        if value == 1:
             return queryset.filter(shopping_cart__user=self.request.user)
-        return queryset
+        # if value and self.request.user.is_authenticated:
+            # return queryset.filter(shopping_cart__user=self.request.user)
+        # return queryset
