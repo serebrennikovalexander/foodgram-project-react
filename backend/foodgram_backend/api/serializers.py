54b29c4e90e3919ba4abfbe1dffcd8ded9260d40
Александр Серебренников <serebrenn1kov.alexander@yandex.ru>
2023-06-08 23:25:23 +0300

    Third review correction of the reviewer's remarks

--- backend/foodgram_backend/api/serializers.py
+++ backend/foodgram_backend/api/serializers.py
@@ -28,10 +28,10 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
     def get_is_subscribed(self, obj):
         user = self.context.get('request').user
-        return Follow.objects.filter(
-            user=user,
-            author=obj
-        ).exists()
+        return (
+            not user.is_anonymous
+            and Follow.objects.filter(user=user, author=obj).exists()
+        )
 
 
 class MainTagSerializer(serializers.ModelSerializer):
@@ -118,17 +118,23 @@ class ReadRecipeSerializer(serializers.ModelSerializer):
 
     def get_is_favorited(self, obj):
         user = self.context.get('request').user
-        return Favorite.objects.filter(
-            user=user.id,
-            recipe=obj.id
-        ).exists()
+        return (
+            not user.is_anonymous
+            and Favorite.objects.filter(
+                user=user.id,
+                recipe=obj.id
+            ).exists()
+        )
 
     def get_is_in_shopping_cart(self, obj):
         user = self.context.get('request').user
-        return Recipe.objects.filter(
-            shopping_cart__user=user,
-            id=obj.id
-        ).exists()
+        return (
+            not user.is_anonymous
+            and Recipe.objects.filter(
+                shopping_cart__user=user,
+                id=obj.id
+            ).exists()
+        )
 
 
 class CreateRecipeSerializer(serializers.ModelSerializer):
